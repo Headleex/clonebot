20e7dc229091fed004d4cf4e066f5869ed167d11
Raman Keraz <romakerez2002@icloud.com>
2022-08-19 20:29:56 +0200

    Update credentials

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "AgAnQ9IAY1nu_L8j2-RMLMWIb-0nGybytvpyDQ0DU6zdkMbuo3MfFdvhQx3I--KB7cIGtT9yhxujMy8Nk0DqTdgQ9BoDs0Zi-Tqgbf_Mj6FIo9WDJQmhOhd7Le0K6tNuLFH6_Z4f61O0Dv3Kto6T-FdZ_Ma2pjxPwIF7HPJxyyg9eNuVYz9WanBc2NxKpEu5Y6KCJ5O-y1ZPfUGxlFSRFn-v2dZNIcFgr_QToj30F3mfXKOGtNj2NEpkmllT4ZrNyMg04jme7sRcR7AVybP9W5aT3cFmEWde_CLcZlrCK1AKYH2HLplBb7WjTRW2kUFXFgfFQICHngw_esSy__quF5B5v0FKswAAAAAaLxdBAA")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BADy3uQAjlJq-u6JJW5BxhJNfLvdv9GWnTdEdAIPi7OlEXd1j-D1aBmQVfhh0R4t1zueTuZoB9PbVGENzcuIlFMGQvhm6OiEXrcQK_kN1GNEairvL5GnRFskvSTl9hJ_vVP05uWw3vjFQ4GXkl7vNCcSL46XmqIKRis-NWbRGoWGxE2HdW_T_6utOQw7lbcdcXWO6-BNt04AKeoShB87HPMeAxu6ToSFILIZxMfFCuH0RsGeQub-L9Z3xGayMWYTDsc9frcC5YfFI4rNmHXXex1STxDc3b7hPg7IuTmJmB3LBEVnBCd5cfKz3p3QOZJuHjDjojOH0y4f0QEaV3Fu078qDHUkeAAAAAFEV819AA")
 
     # Database URI
     DB_URI = os.environ.get("DATABASE_URL", "")
